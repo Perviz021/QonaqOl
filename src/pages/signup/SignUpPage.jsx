46dccc23dd5527bfebf4ba6f6f230e7ad31cc490
Parviz Namazli <perviznamazli021@gmail.com>
2024-02-22 21:57:09 +0400

    fix: Sign Up functionality

--- src/pages/signup/SignUpPage.jsx
+++ src/pages/signup/SignUpPage.jsx
@@ -1,26 +1,27 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { FaGoogle, FaEye, FaEyeSlash } from "react-icons/fa";
-
 import { signupBg } from "../../assets";
 
 function SignUpPage() {
-  const [imageLoaded, setImageLoaded] = useState(false);
-  const [fullname, setFullname] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [fieldsFilled, setFieldsFilled] = useState(false);
-  const [fullnameValid, setFullnameValid] = useState(true);
-  const [emailValid, setEmailValid] = useState(true);
-  const [passwordValid, setPasswordValid] = useState(true);
-  const [confirmPasswordValid, setConfirmPasswordValid] = useState(true);
-  const [showPassword, setShowPassword] = useState(false);
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-  const [passwordMatch, setPasswordMatch] = useState(false);
+  const [formState, setFormState] = useState({
+    imageLoaded: false,
+    fullname: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    fieldsFilled: false,
+    fullnameValid: true,
+    emailValid: true,
+    passwordValid: true,
+    confirmPasswordValid: true,
+    showPassword: false,
+    showConfirmPassword: false,
+    passwordMatch: false,
+  });
 
   const handleImageLoad = () => {
-    setImageLoaded(true);
+    setFormState((prevState) => ({ ...prevState, imageLoaded: true }));
   };
 
   const isEmailValid = (email) => {
@@ -32,46 +33,78 @@ function SignUpPage() {
     return password.length <= 10;
   };
 
-  const handleFullnameChange = (e) => {
-    const value = e.target.value;
-    setFullname(value.slice(0, 40)); // Limit fullname to 40 characters
-    setFieldsFilled(value && email && password && confirmPassword);
-    setFullnameValid(value.length <= 40); // Validate fullname length
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setFormState((prevState) => ({
+      ...prevState,
+      [id]:
+        id === "email" ? value : value.slice(0, id === "fullname" ? 40 : 10),
+      fieldsFilled:
+        id !== "confirmPassword"
+          ? {
+              ...prevState.fieldsFilled,
+              [id]: value !== "",
+            }
+          : prevState.fieldsFilled,
+      [`${id}Valid`]:
+        id === "email"
+          ? isEmailValid(value)
+          : id === "password"
+          ? isPasswordValid(value)
+          : id === "confirmPassword"
+          ? value === formState.password
+          : value.length <= (id === "fullname" ? 40 : 10),
+      passwordMatch:
+        id === "password"
+          ? value === formState.confirmPassword
+          : id === "confirmPassword"
+          ? value === formState.password
+          : prevState.passwordMatch,
+    }));
   };
 
-  const handleEmailChange = (e) => {
-    const value = e.target.value;
-    setEmail(value);
-    setFieldsFilled(fullname && value && password && confirmPassword);
-    setEmailValid(isEmailValid(value));
+  const toggleVisibility = (field) => {
+    setFormState((prevState) => ({
+      ...prevState,
+      [field]: !prevState[field],
+    }));
   };
 
-  const handlePasswordChange = (e) => {
-    const value = e.target.value;
-    setPassword(value);
-    setFieldsFilled(fullname && email && value && confirmPassword);
-    setPasswordValid(isPasswordValid(value));
-    setPasswordMatch(value === confirmPassword); // Check if passwords match
-  };
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Prevent default form submission
 
-  const handleConfirmPasswordChange = (e) => {
-    const value = e.target.value;
-    setConfirmPassword(value);
-    setFieldsFilled(fullname && email && password && value);
-    setConfirmPasswordValid(value === password); // Validate confirm password match
-    setPasswordMatch(password === value); // Check if passwords match
-  };
+    try {
+      const response = await fetch(
+        "https://qonaqol.onrender.com/api/v1/auth/signup",
+        {
+          method: "POST",
+          mode: "no-cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            fullname: formState.fullname,
+            email: formState.email,
+            password: formState.password,
+            confirmPassword: formState.confirmPassword,
+          }),
+        }
+      );
 
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
-
-  const toggleConfirmPasswordVisibility = () => {
-    setShowConfirmPassword(!showConfirmPassword);
+      if (response.ok) {
+        // Handle success
+        console.log("User signed up successfully!");
+      } else {
+        // Handle error
+        console.error("Error signing up:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error signing up:", error.message);
+    }
   };
 
   return (
-    <div style={{ visibility: imageLoaded ? "visible" : "hidden" }}>
+    <div style={{ visibility: formState.imageLoaded ? "visible" : "hidden" }}>
       <div className="flex h-screen">
         {/* Left Side (Image) */}
         <div className="w-1/2">
@@ -79,7 +112,7 @@ function SignUpPage() {
             src={signupBg}
             alt=""
             className={`w-full h-full object-cover ${
-              imageLoaded ? "" : "hidden"
+              formState.imageLoaded ? "" : "hidden"
             }`}
             onLoad={handleImageLoad}
           />
@@ -95,107 +128,109 @@ function SignUpPage() {
                 Qeydiyyatdan keç
               </h2>
 
-              {/* Fullname Input */}
-              <div
-                className={`mb-[20px] ${
-                  !fullnameValid ? "border-red-500" : ""
-                }`}
-              >
-                <input
-                  className={`appearance-none rounded-[8px] text-[16px] font-[400] w-full py-[10px] px-[20px] text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-[#f2f2f2] focus:border-transparent focus:ring-0 ${
-                    !fullnameValid ? "border-red-500" : ""
-                  }`}
-                  id="fullname"
-                  type="text"
-                  value={fullname}
-                  onChange={handleFullnameChange}
-                  placeholder="Ad Soyad"
-                />
-              </div>
-
-              {/* Email Input */}
-              <div
-                className={`mb-[20px] ${!emailValid ? "border-red-500" : ""}`}
-              >
-                <input
-                  className={`appearance-none rounded-[8px] text-[16px] font-[400] w-full py-[10px] px-[20px] text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-[#f2f2f2] focus:border-transparent focus:ring-0 ${
-                    !emailValid ? "border-red-500" : ""
-                  }`}
-                  id="email"
-                  type="email"
-                  value={email}
-                  onChange={handleEmailChange}
-                  placeholder="E-poçt"
-                />
-              </div>
-
-              {/* Password Input */}
-              <div
-                className={`mb-[20px] relative ${
-                  !passwordValid ? "border-red-500" : ""
-                }`}
-              >
-                <input
-                  className={`appearance-none rounded-[8px] text-[16px] font-[400] w-full py-[10px] px-[20px] pr-[40px] text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-[#f2f2f2] focus:border-transparent focus:ring-0 ${
-                    !passwordValid ? "border-red-500" : ""
-                  }`}
-                  id="password"
-                  type={showPassword ? "text" : "password"}
-                  value={password}
-                  onChange={handlePasswordChange}
-                  maxLength={10}
-                  placeholder="Şifrə"
-                />
+              <form onSubmit={handleSubmit}>
+                {" "}
+                {/* Added onSubmit handler */}
+                {/* Fullname Input */}
                 <div
-                  className="absolute top-0 right-0 h-full flex items-center pr-[10px] cursor-pointer"
-                  onClick={togglePasswordVisibility}
-                >
-                  {showPassword ? <FaEyeSlash /> : <FaEye />}
-                </div>
-              </div>
-
-              {/* Confirm Password */}
-              <div
-                className={`mb-[24px] relative ${
-                  !confirmPasswordValid ? "border-red-500" : ""
-                }`}
-              >
-                <input
-                  className={`appearance-none rounded-[8px] text-[16px] font-[400] w-full py-[10px] px-[20px] pr-[40px] text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-[#f2f2f2] focus:border-transparent focus:ring-0 ${
-                    !confirmPasswordValid ? "border-red-500" : ""
+                  className={`mb-[20px] ${
+                    !formState.fullnameValid ? "border-red-500" : ""
                   }`}
-                  id="confirm-password"
-                  type={showConfirmPassword ? "text" : "password"}
-                  value={confirmPassword}
-                  onChange={handleConfirmPasswordChange}
-                  maxLength={10}
-                  placeholder="Şifrəni təsdiqləyin"
-                />
-                <div
-                  className="absolute top-0 right-0 h-full flex items-center pr-[10px] cursor-pointer"
-                  onClick={toggleConfirmPasswordVisibility}
                 >
-                  {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
+                  <input
+                    className={`appearance-none rounded-[8px] text-[16px] font-[400] w-full py-[10px] px-[20px] text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-[#f2f2f2] focus:border-transparent focus:ring-0 ${
+                      !formState.fullnameValid ? "border-red-500" : ""
+                    }`}
+                    id="fullname"
+                    type="text"
+                    value={formState.fullname}
+                    onChange={handleInputChange}
+                    placeholder="Ad Soyad"
+                  />
                 </div>
-              </div>
-
-              {/* Login Button */}
-              {fieldsFilled && passwordMatch && (
-                <div className="flex flex-col items-center justify-center space-y-[8px]">
-                  <button
-                    className={`bg-[#FFCE00] text-black text-[16px] font-normal h-[44px] w-full rounded-[8px] focus:outline-none focus:shadow-outline`}
-                    type="button"
+                {/* Email Input */}
+                <div
+                  className={`mb-[20px] ${
+                    !formState.emailValid ? "border-red-500" : ""
+                  }`}
+                >
+                  <input
+                    className={`appearance-none rounded-[8px] text-[16px] font-[400] w-full py-[10px] px-[20px] text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-[#f2f2f2] focus:border-transparent focus:ring-0 ${
+                      !formState.emailValid ? "border-red-500" : ""
+                    }`}
+                    id="email"
+                    type="email"
+                    value={formState.email}
+                    onChange={handleInputChange}
+                    placeholder="E-poçt"
+                  />
+                </div>
+                {/* Password Input */}
+                <div
+                  className={`mb-[20px] relative ${
+                    !formState.passwordValid ? "border-red-500" : ""
+                  }`}
+                >
+                  <input
+                    className={`appearance-none rounded-[8px] text-[16px] font-[400] w-full py-[10px] px-[20px] pr-[40px] text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-[#f2f2f2] focus:border-transparent focus:ring-0 ${
+                      !formState.passwordValid ? "border-red-500" : ""
+                    }`}
+                    id="password"
+                    type={formState.showPassword ? "text" : "password"}
+                    value={formState.password}
+                    onChange={handleInputChange}
+                    maxLength={10}
+                    placeholder="Şifrə"
+                  />
+                  <div
+                    className="absolute top-0 right-0 h-full flex items-center pr-[10px] cursor-pointer"
+                    onClick={() => toggleVisibility("showPassword")}
                   >
-                    Qeydiyyat
-                  </button>
-                  <p className="text-center flex items-center text-[12px] leading-[20px]">
-                    Artıq qeydiyyatdan keçmisiniz?{" "}
-                    <NavLink to="/login" className="underline ml-1">
-                      Daxil olun
-                    </NavLink>
-                  </p>
+                    {formState.showPassword ? <FaEyeSlash /> : <FaEye />}
+                  </div>
                 </div>
-              )}
+                {/* Confirm Password */}
+                <div
+                  className={`mb-[24px] relative ${
+                    !formState.confirmPasswordValid ? "border-red-500" : ""
+                  }`}
+                >
+                  <input
+                    className={`appearance-none rounded-[8px] text-[16px] font-[400] w-full py-[10px] px-[20px] pr-[40px] text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-[#f2f2f2] focus:border-transparent focus:ring-0 ${
+                      !formState.confirmPasswordValid ? "border-red-500" : ""
+                    }`}
+                    id="confirmPassword"
+                    type={formState.showConfirmPassword ? "text" : "password"}
+                    value={formState.confirmPassword}
+                    onChange={handleInputChange}
+                    maxLength={10}
+                    placeholder="Şifrəni təsdiqləyin"
+                  />
+                  <div
+                    className="absolute top-0 right-0 h-full flex items-center pr-[10px] cursor-pointer"
+                    onClick={() => toggleVisibility("showConfirmPassword")}
+                  >
+                    {formState.showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
+                  </div>
+                </div>
+                {/* Sign Up Button */}
+                {formState.fieldsFilled && formState.passwordMatch && (
+                  <div className="flex flex-col items-center justify-center space-y-[8px]">
+                    <button
+                      className={`bg-[#FFCE00] text-black text-[16px] font-normal h-[44px] w-full rounded-[8px] focus:outline-none focus:shadow-outline`}
+                      type="submit"
+                    >
+                      Qeydiyyat
+                    </button>
+                    <p className="text-center flex items-center text-[12px] leading-[20px]">
+                      Artıq qeydiyyatdan keçmisiniz?{" "}
+                      <NavLink to="/login" className="underline ml-1">
+                        Daxil olun
+                      </NavLink>
+                    </p>
+                  </div>
+                )}
+              </form>
             </div>
 
             {/* Or Separator */}
@@ -218,9 +253,11 @@ function SignUpPage() {
               </button>
               <p className="text-[12px] py-3 text-[#2B2B2B] text-center font-normal">
                 Hesabınıza giriş edərək və ya yeni hesab yaradaraq{" "}
-                <span className="text-[#0074C6]">Xidmət Şərtləri</span>
-                və <span className="#0074C6">Məxfilik Siyasətini</span> qəbul
-                etmiş olursunuz
+                <span className="text-[#0074C6] mr-1">Xidmət Şərtləri</span>
+                və <span className="text-[#0074C6]">
+                  Məxfilik Siyasətini
+                </span>{" "}
+                qəbul etmiş olursunuz
               </p>
             </div>
           </div>
