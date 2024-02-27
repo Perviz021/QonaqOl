256efded7f4d360a9256d31dd36a7037ebaf60c9
Parviz Namazli <perviznamazli021@gmail.com>
2024-02-27 20:10:46 +0400

    feat: Login page

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import ErrorPage from "./pages/error/ErrorPage.jsx";
 import SignUpPage from "./pages/signup/SignUpPage.jsx";
 import GiftCardsPage from "./pages/giftcard/GiftCardsPage.jsx";
 import CreateEvent from "./pages/event/CreateEvent.jsx";
+import LoginPage from "./pages/login/LoginPage.jsx";
 
 const router = createBrowserRouter([
   {
@@ -21,7 +22,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/login",
-    element: <HomePage />,
+    element: <LoginPage />,
     errorElement: <ErrorPage />,
   },
   {
