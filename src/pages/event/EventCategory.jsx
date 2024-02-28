243d2a6a2067874de3bbdddf0842c67ad878cf69
Parviz Namazli <perviznamazli021@gmail.com>
2024-02-28 21:17:30 +0400

    feat: Event Category component

---
+++ src/pages/event/EventCategory.jsx
@@ -0,0 +1,73 @@
+import React, { useState, useEffect, useRef } from "react";
+import { chevronDown, chevronUp } from "../../assets";
+
+const EventCategory = ({ options }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="mt-[40px]">
+      <h4 className="font-[500] text-[20px] leading-[28px] mb-[12px]">
+        Kateqoriya
+      </h4>
+      <div className="text-left relative" ref={dropdownRef}>
+        <div>
+          <button
+            type="button"
+            className={`inline-flex justify-between items-center w-full ${
+              isOpen ? "rounded-t-[8px]" : "rounded-[8px]"
+            } border-transparent focus:border-transparent focus:ring-0 px-[20px] h-[44px] bg-[#f2f2f2] text-[16px] font-[400] focus:outline-none`}
+            onClick={toggleMenu}
+          >
+            <span
+              className={`${selectedOption ? "text-black" : "text-[#919191]"}`}
+            >
+              {selectedOption ? selectedOption : "Kateqroyanı seçin"}
+            </span>
+            <img src={isOpen ? chevronUp : chevronDown} alt="" />
+          </button>
+        </div>
+
+        {isOpen && (
+          <div className="origin-top-right absolute right-0 w-full rounded-b-[8px] border-transparent focus:border-transparent focus:ring-0 bg-[#f2f2f2]  focus:outline-none z-20 py-[20px] space-y-[16px]">
+            {options.map((option, index) => (
+              <button
+                key={index}
+                onClick={() => handleOptionClick(option)}
+                className="block w-full text-left pl-[20px] text-[16px] text-[#616161] hover:text-gray-900"
+                role="menuitem"
+              >
+                {option}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default EventCategory;
