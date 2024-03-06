5c1c23b6ade0753207e568396825d3c2ba6249e4
Parviz Namazli <perviznamazli021@gmail.com>
2024-03-06 12:09:23 +0400

    fix: Event Time

--- src/pages/event/EventTime.jsx
+++ src/pages/event/EventTime.jsx
@@ -1,13 +1,14 @@
+// EventTime.js
 import React, { useState } from "react";
 import { clock } from "../../assets";
 
-const EventTime = () => {
-  const [showStartInput, setShowStartInput] = useState(false);
-  const [showEndInput, setShowEndInput] = useState(false);
+const EventTime = ({ onTimeChange }) => {
+  const [showTimeInputs, setShowTimeInputs] = useState(false);
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
 
-  const handleStartInputClick = () => {
-    setShowStartInput(!showStartInput);
-    setShowEndInput(!showEndInput); // Close the end input if open
+  const handleTimeInputClick = () => {
+    setShowTimeInputs(!showTimeInputs);
   };
 
   const startTimeOptions = [];
@@ -22,44 +23,71 @@ const EventTime = () => {
     endTimeOptions.push(`${hour}:30`);
   }
 
+  const handleStartTimeChange = (e) => {
+    const value = e.target.value;
+    setStartTime(value);
+    onTimeChange(startTime, value); // Pass both start and end times to the parent component
+  };
+
+  const handleEndTimeChange = (e) => {
+    const value = e.target.value;
+    setEndTime(value);
+    onTimeChange(value, endTime); // Pass both start and end times to the parent component
+  };
+
   return (
     <div className="w-[45%]">
       <h4 className="font-[500] text-[20px] leading-[28px] mb-[12px]">
         Tədbirin saatı
       </h4>
-      <button className="inline-flex justify-between items-center rounded-[8px] mb-[17px] bg-[#f2f2f2] w-full h-[44px] px-[20px]">
-        <span
-          className="font-[16px] text-[#919191]"
-          onClick={handleStartInputClick}
-        >
+      <button
+        onClick={handleTimeInputClick}
+        className="inline-flex justify-between items-center rounded-[8px] mb-[17px] bg-[#f2f2f2] w-full h-[44px] px-[20px]"
+      >
+        <span className="font-[16px] text-[#919191]">
           Saat aralığını qeyd edin
         </span>
         <span>
           <img src={clock} alt="" />
         </span>
       </button>
-      {showStartInput && (
-        <div className="flex justify-evenly">
-          <select
-            className="rounded-[8px] bg-[#f2f2f2] w-[136px] h-[44px] px-[20px] border-transparent focus:border-transparent focus:ring-0"
-            defaultValue=""
-          >
-            {startTimeOptions.map((time, index) => (
-              <option key={index} value={time}>
-                {time}
-              </option>
-            ))}
-          </select>
-          <select
-            className="rounded-[8px] bg-[#f2f2f2] w-[136px] h-[44px] px-[20px] border-transparent focus:border-transparent focus:ring-0"
-            defaultValue=""
-          >
-            {endTimeOptions.map((time, index) => (
-              <option key={index} value={time}>
-                {time}
-              </option>
-            ))}
-          </select>
+      {showTimeInputs && (
+        <div className="flex justify-between">
+          <div className="w-[45%]">
+            <p className="text-[#919191] bg-[#f2f2f2] text-[12px] flex flex-col rounded-[8px] mb-2 p-2 pl-3">
+              <span>Başlayır</span>
+            </p>
+
+            <select
+              className="rounded-[8px] text-[12px] bg-[#f2f2f2] w-full h-[44px] px-[20px] border-transparent focus:border-transparent focus:ring-0"
+              value={startTime}
+              onChange={handleStartTimeChange}
+            >
+              <option value="">Saatı qeyd edin</option>
+              {startTimeOptions.map((time, index) => (
+                <option key={index} value={time}>
+                  {time}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="w-[45%]">
+            <p className="text-[#919191] bg-[#f2f2f2] text-[12px] flex flex-col rounded-[8px] mb-2 p-2 pl-3">
+              <span>Bitir</span>
+            </p>
+            <select
+              className="rounded-[8px] text-[12px] bg-[#f2f2f2] w-full h-[44px] px-[20px] border-transparent focus:border-transparent focus:ring-0"
+              value={endTime}
+              onChange={handleEndTimeChange}
+            >
+              <option value="">Saatı qeyd edin</option>
+              {endTimeOptions.map((time, index) => (
+                <option key={index} value={time}>
+                  {time}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
       )}
     </div>
