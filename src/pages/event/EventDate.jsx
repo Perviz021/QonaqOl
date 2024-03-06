ea77c5130ca9e0cba58ab82231ed2c14128afd87
Parviz Namazli <perviznamazli021@gmail.com>
2024-03-06 12:08:13 +0400

    fix: Event Date

--- src/pages/event/EventDate.jsx
+++ src/pages/event/EventDate.jsx
@@ -42,11 +42,23 @@ const CustomDatePicker = ({ selectedDate, onChange }) => {
   );
 };
 
-const EventDate = () => {
+const EventDate = ({ onDateChange }) => {
   const [selectedDate, setSelectedDate] = useState(null);
 
   const handleDateChange = (date) => {
-    setSelectedDate(date);
+    if (date) {
+      const formattedDate = formatDate(date); // Format the date
+      setSelectedDate(date); // Set the selected date in the state
+      onDateChange(formattedDate); // Pass the formatted date to the parent component
+    }
+  };
+
+  // Function to format date as "year-month-day"
+  const formatDate = (date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
   };
 
   return (
