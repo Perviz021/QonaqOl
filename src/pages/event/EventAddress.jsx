5f27c9af63083f2890828cb481a3d719f6426d3f
Parviz Namazli <perviznamazli021@gmail.com>
2024-03-06 12:09:05 +0400

    fix: Event Address

--- src/pages/event/EventAddress.jsx
+++ src/pages/event/EventAddress.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 
-const EventAddress = () => {
+const EventAddress = ({ onAddressChange }) => {
   const [addressValue, setAddressValue] = useState("");
+
+  const handleChange = (e) => {
+    const address = e.target.value;
+    setAddressValue(address);
+    onAddressChange(address); // Pass the address value to the parent component
+  };
+
   return (
     <div>
       <h4 className="font-[500] text-[20px] leading-[28px] mb-[12px]">Ünvan</h4>
       <input
         type="text"
         value={addressValue}
-        onChange={(e) => setAddressValue(e.target.value)}
+        onChange={handleChange}
         placeholder="Küç, prospekt, mənzil, ev"
         className="text-[16px] bg-[#f2f2f2] w-full border-transparent focus:border-transparent focus:ring-0 rounded-[8px] px-[20px]"
       />
