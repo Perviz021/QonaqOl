e0ae7415141ca5937e9ef0ca6baf401c8ca40f2f
Parviz Namazli <perviznamazli021@gmail.com>
2024-03-14 10:04:25 +0400

    fix: Giftcard component

--- src/pages/giftcard/GiftCardsPage.jsx
+++ src/pages/giftcard/GiftCardsPage.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import GiftCard from "../../components/widgets/GiftCard";
 import { giftcard1, giftcard2, giftcard3 } from "../../assets";
+import { useForm } from "react-hook-form";
 
 const GiftCardsPage = () => {
+  const [openPopup, setOpenPopup] = useState(false);
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const showPopup = () => {
+    setOpenPopup(true);
+  };
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  const handleInputChange = () => {
+    setIsFormValid(errors.length === 0);
+  };
+
   return (
     <div>
       <div className="text-center mt-[120px] w-[800px] mx-auto space-y-[36px] mb-[80px]">
@@ -17,9 +39,9 @@ const GiftCardsPage = () => {
         </p>
       </div>
       <div className="w-full px-[100px] flex justify-between gap-[18px] mb-[100px]">
-        <GiftCard imgSrc={giftcard1} />
-        <GiftCard imgSrc={giftcard2} />
-        <GiftCard imgSrc={giftcard3} />
+        <GiftCard imgSrc={giftcard1} showPopup={showPopup} /> {/* AZN_50*/}
+        <GiftCard imgSrc={giftcard2} showPopup={showPopup} /> {/* AZN_100*/}
+        <GiftCard imgSrc={giftcard3} showPopup={showPopup} /> {/* AZN_200*/}
       </div>
       <div className="w-[full] px-[100px] mb-[400px]">
         <h1 className="unbounded unbounded-700 text-[40px] mb-[36px]">
@@ -45,6 +67,60 @@ const GiftCardsPage = () => {
           </div>
         </div>
       </div>
+
+      {openPopup && (
+        <div className="fixed top-0 left-0 size-full flex flex-col justify-center items-center bg-black bg-opacity-50">
+          <div className="p-[40px] bg-white rounded-[8px]">
+            <h2 className="font-[600] text-[24px] text-center mb-[30px]">
+              Sevdiklərinizi sevindirin
+            </h2>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="w-[400px] space-y-[20px] flex flex-col"
+            >
+              <input
+                {...register("to", { required: true })}
+                placeholder="Kimə"
+                className="rounded-[8px] bg-[#f2f2f2] placeholder:opacity-50 py-[10px] px-[20px] border-none outline-none"
+                onChange={handleInputChange}
+              />
+              <input
+                {...register("from", { required: true })}
+                placeholder="Kimdən"
+                className="rounded-[8px] bg-[#f2f2f2] placeholder:opacity-50 py-[10px] px-[20px] border-none outline-none"
+                onChange={handleInputChange}
+              />
+              <input
+                {...register("message")}
+                placeholder="Şəxsi mesaj"
+                className="rounded-[8px] bg-[#f2f2f2] placeholder:opacity-50 py-[10px] px-[20px] border-none outline-none"
+              />
+              <input
+                {...register("contact", {
+                  pattern: /^[0-9]*$/,
+                  required: true,
+                })}
+                type="tel"
+                placeholder="Əlaqə nömrəsi"
+                className="rounded-[8px] bg-[#f2f2f2] placeholder:opacity-50 py-[10px] px-[20px] border-none outline-none"
+                onChange={handleInputChange}
+                onPaste={(e) => e.preventDefault()} // Prevent pasting non-numeric values
+              />
+              <button
+                type="submit"
+                className={`mt-[24px] rounded-[8px] h-[48px] ${
+                  isFormValid
+                    ? "bg-[#FFCE00] cursor-pointer"
+                    : "bg-[#F1DD8B] cursor-not-allowed"
+                }`}
+                disabled={!isFormValid}
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
