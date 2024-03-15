ebcc50a18e1c7b711340f88dd7e7bce0a6c4ff49
Parviz Namazli <perviznamazli021@gmail.com>
2024-03-15 15:34:53 +0400

    fix: active nav link functionality

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -1,13 +1,19 @@
 import { Link } from "react-router-dom";
 
-function Logo({ color }) {
+function Logo({ color, clearActiveLink }) {
   const textColor = color ? color : "#313131";
+
+  const handleLogoClick = () => {
+    // Call the function to clear active link state in the parent component
+    clearActiveLink();
+  };
   return (
     <div className="px-[12px] py-[8px]">
       <Link
         to="/"
         className={`text-[18px] leading-[28px] 
           text-${[textColor]} unbounded unbounded-700`}
+        onClick={handleLogoClick}
       >
         Qonaqol.az
       </Link>
