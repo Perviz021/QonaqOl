22c5de7924772b8616ae47128a3ff1919b79bb28
Parviz Namazli <perviznamazli021@gmail.com>
2024-03-20 21:25:27 +0400

    refactor: Login and Sign up pages

---
+++ src/utils/authUtils.jsx
@@ -0,0 +1,249 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../lib/firebase";
+const provider = new GoogleAuthProvider();
+
+export const isEmailValid = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+export const isPasswordValid = (value) => {
+  return value.length <= 10;
+};
+
+export const handleImageLoad = (setFormState) => {
+  setFormState((prevState) => ({ ...prevState, imageLoaded: true }));
+};
+
+export const togglePasswordVisibility = (field, setFormState) => {
+  setFormState((prevData) => ({
+    ...prevData,
+    [field]: !prevData[field],
+  }));
+};
+
+export const useRefreshToken = async (navigate) => {
+  try {
+    const refreshToken = localStorage.getItem("refreshToken");
+    const response = await fetch(
+      "https://qonaqol.onrender.com/qonaqol/api/v1/auth/refresh",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          refreshToken: refreshToken,
+        }),
+      }
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+      const newAccessToken = data.accessToken;
+      const newRefreshToken = data.refreshToken;
+      // Update tokens in local storage
+      localStorage.setItem("accessToken", newAccessToken);
+      localStorage.setItem("refreshToken", newRefreshToken);
+      return newAccessToken;
+    } else {
+      console.error("Error refreshing token:", response.statusText);
+      // Optionally, handle invalid or expired refresh token
+      // For example, clear tokens from local storage and redirect to login page
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("refreshToken");
+      navigate("/login");
+    }
+  } catch (error) {
+    console.error("Error refreshing token:", error.message);
+    // Handle network errors or other exceptions
+    // Optionally, clear tokens from local storage and redirect to login page
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    navigate("/login");
+  }
+};
+
+export const googleSignup = async (
+  e,
+  type,
+  formState,
+  setFormState,
+  navigate
+) => {
+  try {
+    const data = await signInWithPopup(auth, provider);
+    const credential = GoogleAuthProvider.credentialFromResult(data);
+    const token = credential.accessToken;
+    const user = data.user;
+
+    if (type === "signup")
+      handleSubmit(e, user, formState, setFormState, navigate);
+    else if (type === "login")
+      handleSignIn(e, user, formState, setFormState, navigate);
+  } catch (error) {
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    console.error(credential);
+  }
+};
+
+// SignUp
+export const handleSubmit = async (
+  e,
+  user = null,
+  formState,
+  setFormState,
+  navigate
+) => {
+  e.preventDefault();
+
+  setFormState((prevData) => ({
+    ...prevData,
+    loading: true,
+  }));
+  try {
+    let payload = {};
+    if (!user) {
+      // If user is not provided (normal sign up)
+      payload = {
+        fullName: formState.fullName,
+        email: formState.email,
+        password: formState.password,
+        confirmPassword: formState.confirmPassword,
+      };
+    } else {
+      // If user is provided (sign up with Google)
+      payload = {
+        fullName: user?.displayName,
+        email: user?.email,
+        password: user?.uid.slice(0, 10), // Using user UID as password
+        confirmPassword: user?.uid.slice(0, 10), // Using user UID as confirmation password
+      };
+    }
+
+    const response = await fetch(
+      "https://qonaqol.onrender.com/qonaqol/api/v1/auth/signup",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      }
+    );
+
+    if (response.ok) {
+      // Handle success
+      console.log("User signed up successfully!");
+      const data = await response.json();
+      const { userId, tokenPair } = data;
+      const { accessToken, refreshToken } = tokenPair;
+
+      // Store userId, accessToken, and refreshToken in local storage
+      localStorage.setItem("userId", userId);
+      localStorage.setItem("accessToken", accessToken);
+      localStorage.setItem("refreshToken", refreshToken);
+
+      navigate("/"); // Navigate to login page using useNavigate
+    } else {
+      if (response.status === 401) {
+        // If token is expired or invalid, try refreshing the token
+        const newAccessToken = await useRefreshToken(navigate);
+        if (newAccessToken) {
+          // If a new access token is obtained, retry the signup
+          return handleSubmit(e);
+        } else {
+          // If unable to refresh token, handle error appropriately
+          console.error("Unable to refresh token. Please try again later.");
+        }
+      } else if (response.status === 404) {
+        handleSignIn(e, user, formState, setFormState, navigate);
+      } else {
+        // Handle other errors
+        console.error("Error signing up:", response.statusText);
+      }
+    }
+  } catch (error) {
+    console.error("Error signing up:", error.message);
+  } finally {
+    setFormState((prevData) => ({
+      ...prevData,
+      loading: false,
+    }));
+  }
+};
+
+// Login
+export const handleSignIn = async (
+  e,
+  user = null,
+  formState,
+  setFormState,
+  navigate
+) => {
+  e.preventDefault();
+
+  setFormState((prevData) => ({
+    ...prevData,
+    loading: true,
+  }));
+
+  try {
+    let payload = {};
+    if (!user) {
+      // If user is not provided (normal sign up)
+      payload = {
+        email: formState.email,
+        password: formState.password,
+      };
+    } else {
+      // If user is provided (sign up with Google)
+      payload = {
+        email: user?.email,
+        password: user?.uid.slice(0, 10), // Using user UID as password
+      };
+    }
+
+    const response = await fetch(
+      "https://qonaqol.onrender.com/qonaqol/api/v1/auth/signin",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      }
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+      const { userId, tokenPair } = data;
+      const { accessToken, refreshToken } = tokenPair;
+
+      // Store userId, accessToken, and refreshToken in local storage
+      localStorage.setItem("userId", userId);
+      localStorage.setItem("accessToken", accessToken);
+      localStorage.setItem("refreshToken", refreshToken);
+
+      navigate("/"); // Navigate to home page
+    } else if (response.status === 403) {
+      const newAccessToken = await useRefreshToken(navigate);
+      if (newAccessToken) {
+        return handleSignIn(); // Retry sign-in with new access token
+      } else {
+        console.error("Unable to refresh token. Please try again later.");
+      }
+    } else if (response.status === 404) {
+      handleSubmit(e, user, formState, setFormState, navigate);
+    } else {
+      console.error("Sign-in failed");
+    }
+  } catch (error) {
+    console.error("Error signing in:", error);
+  } finally {
+    setFormState((prevData) => ({
+      ...prevData,
+      loading: false,
+    }));
+  }
+};
