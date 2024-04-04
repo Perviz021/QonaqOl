5e2dba711a50d0da41f6cd9d63db83e1355e63f4
Parviz Namazli <perviznamazli021@gmail.com>
2024-04-05 00:57:18 +0400

    fix: scroll to top

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AboutCard from "./AboutCard";
 
 const About = () => {
+  useEffect(() => {
+    // Smooth scrolling:
+    // const scrollToTop = () => {
+    //   const c = document.documentElement.scrollTop || document.body.scrollTop;
+    //   if (c > 0) {
+    //     window.requestAnimationFrame(scrollToTop);
+    //     window.scrollTo(0, c - c / 8); // Adjust the divisor to control scrolling speed
+    //   }
+    // };
+    // scrollToTop();
+
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="w-[820px] mx-auto mt-[140px] mb-[400px] space-y-[36px]">
       <AboutCard headerText="Biz kimik?" />
