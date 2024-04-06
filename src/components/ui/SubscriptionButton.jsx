54237ac7dda488c2ae838bf57eb65cad6097b2fe
Parviz Namazli <perviznamazli021@gmail.com>
2024-04-07 01:40:06 +0400

    feat: responsive design

--- src/components/ui/SubscriptionButton.jsx
+++ src/components/ui/SubscriptionButton.jsx
@@ -3,14 +3,14 @@ import { directbox } from "../../assets";
 const SubscriptionButton = () => {
   return (
     <>
-      <button className="inline-flex items-center w-[90%]">
+      <button className="inline-flex items-center lg:w-[90%]">
         <input
           type="text"
           placeholder="Email ünvanı"
-          className="px-[20px] py-[12px] flex-1 bg-white text-[#000] border-none border-transparent focus:border-transparent focus:ring-0 placeholder:text-[16px] placeholder:font-[400] placeholder:text-[#474744]"
+          className="px-[20px] py-[9px] lg:py-[12px] flex-1 bg-white text-[#000] border-none border-transparent focus:border-transparent focus:ring-0 text-[14px] lg:placeholder:text-[16px] placeholder:font-[400] placeholder:text-[#474744] rounded-l-[8px]"
         />
-        <span className="flex items-center space-x-[8px] px-[54px] bg-[#FFCE00] h-[48px] rounded-r-[8px]">
-          <img src={directbox} alt="" />
+        <span className="flex items-center space-x-[8px] text-[14px] lg:text-[16px] px-[10px] lg:px-[54px] bg-[#FFCE00] h-[40px] lg:h-[48px] rounded-r-[8px]">
+          <img src={directbox} alt="" className="size-[20px] lg:size-[25px]" />
           Abunə ol
         </span>
       </button>
--- src/pages/home/Subscribe.jsx
+++ src/pages/home/Subscribe.jsx
@@ -1,17 +1,40 @@
+import { useEffect, useState } from "react";
 import SubscriptionButton from "../../components/ui/SubscriptionButton";
-import { borderYellow } from "../../assets";
 
 const Subscribe = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 640); // Adjust the threshold as needed
+    };
+
+    // Set initial screen size
+    handleResize();
+
+    // Listen for window resize events
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <section className="bg-[#F2F2F2] mt-[150px] py-[200px]">
-      <div className="w-[720px] mx-auto text-center">
-        <h1 className="unbounded unbounded-700 text-[60px] relative">
+    <section className="w-full bg-[#F2F2F2] mt-[20px] lg:mt-[150px] px-[20px] py-[175px] lg:py-[200px]">
+      <div className="w-full lg:w-[720px] mx-auto text-center">
+        <h1 className="unbounded unbounded-700 text-[28px] lg:text-[60px] relative w-full">
           Yeni tədbirlərdən ilk{" "}
-          <span className="bg-border-yellow relative z-10">
+          <span
+            className={`${
+              isSmallScreen ? "bg-border-yellow" : "bg-border-yellow-lg"
+            } relative z-10`}
+          >
             sən xəbərdar ol
           </span>
         </h1>
-        <p className="mt-[30px] mb-[24px] text-[16px] font-[400]">
+        <p className="mt-[34px] lg:mt-[30px] mb-[60px] lg:mb-[24px] text-[16px] font-[400] leading-[24px]">
           Bizə abunə ol və yeni gələn hər tədbirdən yerlər tükənməmiş xəbər tut
         </p>
         <SubscriptionButton />
