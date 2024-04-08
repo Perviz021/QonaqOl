0893e3f73ac602798afeb43c9749c9031bf7fb63
Parviz Namazli <perviznamazli021@gmail.com>
2024-04-08 23:31:02 +0400

    fix: responsive design

--- src/components/ui/SubscriptionButton.jsx
+++ src/components/ui/SubscriptionButton.jsx
@@ -16,14 +16,14 @@ const SubscriptionButton = () => {
           placeholder="Email ünvanı"
           className={`${
             isMobile
-              ? "py-[9px] text-[14px] w-[218px]"
+              ? "py-[9px] text-[14px] flex-1"
               : "py-[12px] lg:placeholder:text-[16px] placeholder:font-[400] placeholder:text-[#474744]"
           } px-[20px] bg-white text-[#000] border-none border-transparent focus:border-transparent focus:ring-0 rounded-l-[8px]`}
         />
         <span
           className={`${
             isMobile
-              ? "text-[14px] px-[10px] h-[40px]"
+              ? "text-[14px] px-[10px] h-[40px] w-[102px]"
               : "text-[16px] px-[54px] h-[48px]"
           } flex items-center space-x-[8px] cursor-pointer bg-[#FFCE00] rounded-r-[8px]`}
         >
