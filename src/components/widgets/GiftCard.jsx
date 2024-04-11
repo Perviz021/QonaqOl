9fec4de8eb2ded9900ef9cb126677b3b044d7e88
Parviz Namazli <perviznamazli021@gmail.com>
2024-04-11 17:50:18 +0400

    feat: Gift Cards page reponsive design

--- src/components/widgets/GiftCard.jsx
+++ src/components/widgets/GiftCard.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import { useMediaQuery } from "@uidotdev/usehooks";
 
 const GiftCard = ({ imgSrc, showPopup }) => {
+  const isMobile = useMediaQuery("only screen and (max-width: 480px)");
+
   return (
-    <div className="flex flex-col items-center">
-      <div>
+    <div className={`${isMobile ? "w-full" : ""} flex flex-col items-center`}>
+      <div className={`${isMobile ? "w-full" : ""}`}>
         <img src={imgSrc} alt="" />
       </div>
-      <span className="mt-[24px]">
+      <span className={`${isMobile ? "w-full" : ""} mt-[24px]`}>
         <button
           onClick={showPopup}
-          className="border-[#E5E5E5] border rounded-[8px] px-[70px] h-[44px] shrink-0"
+          className={`${
+            isMobile ? "w-full" : "px-[70px]"
+          } border-[#E5E5E5] border rounded-[8px] h-[44px] shrink-0`}
         >
           İndi al
         </button>
--- src/pages/giftcard/GiftCardsPage.jsx
+++ src/pages/giftcard/GiftCardsPage.jsx
@@ -3,10 +3,12 @@ import GiftCard from "../../components/widgets/GiftCard";
 import { giftcard1, giftcard2, giftcard3 } from "../../assets";
 import { useForm } from "react-hook-form";
 import { FaXmark } from "react-icons/fa6";
+import { useMediaQuery } from "@uidotdev/usehooks";
 
 const GiftCardsPage = () => {
   const [openPopup, setOpenPopup] = useState(false);
   const [isFormValid, setIsFormValid] = useState(false);
+  const isMobile = useMediaQuery("only screen and (max-width: 480px)");
 
   const {
     register,
@@ -31,83 +33,105 @@ const GiftCardsPage = () => {
   }, []);
 
   return (
-    <div>
-      <div className="text-center mt-[120px] w-[800px] mx-auto space-y-[36px] mb-[80px]">
-        <h1 className="unbounded unbounded-700 text-[60px] leading-[68px]">
+    <div className={`${isMobile ? "px-[20px]" : ""}`}>
+      <div
+        className={`${
+          isMobile
+            ? "w-full mt-[80px] mb-[40px]"
+            : "w-[800px] mt-[120px] mb-[80px]"
+        } text-center mx-auto space-y-[36px]`}
+      >
+        <h1
+          className={`${
+            isMobile
+              ? "text-[30px] leading-[40px]"
+              : "text-[60px] leading-[68px]"
+          } unbounded unbounded-700`}
+        >
           Hədiyyə kartları ilə yaxınlarını sevindir
         </h1>
-        <p className="font-normal text-[16px] leading-[24px]">
+        <p className="text-base">
           Hədiyyə kartlarımızla sonsuz əyləncə və səmərəli vaxt hədiyyə edin.
           Məbləğinizi seçin, mesajınızı fərdiləşdirin və dərhal e-poçt
           vasitəsilə çatdırın. Qəbul edən dostlarınız master-klasslardan,
           kamplara qədər 120-dən çox fəaliyyət növündə istifadə etsinlər.
         </p>
       </div>
-      <div className="w-full px-[100px] flex justify-between gap-[18px] mb-[240px]">
+      <div
+        className={`${
+          isMobile
+            ? "flex-col mb-[470px] gap-[36px] justify-center"
+            : "mb-[240px] gap-[18px] justify-between px-[100px]"
+        } w-full flex`}
+      >
         <GiftCard imgSrc={giftcard1} showPopup={showPopup} /> {/* AZN_50*/}
         <GiftCard imgSrc={giftcard2} showPopup={showPopup} /> {/* AZN_100*/}
         <GiftCard imgSrc={giftcard3} showPopup={showPopup} /> {/* AZN_200*/}
       </div>
 
-      {openPopup && (
-        <div className="fixed top-0 left-0 size-full flex flex-col justify-center items-center bg-black bg-opacity-50">
-          <div className="p-[40px] relative bg-white rounded-[8px]">
-            <span
-              className="absolute top-6 cursor-pointer right-4 size-6 "
-              onClick={() => setOpenPopup(false)}
+      <div className="fixed top-0 left-0 size-full flex flex-col justify-center items-center bg-black bg-opacity-50 px-[20px] lg:px-0">
+        <div
+          className={`${
+            isMobile ? "w-full" : ""
+          } p-[40px] relative bg-white rounded-[8px]`}
+        >
+          <span
+            className="absolute top-6 cursor-pointer right-4 size-6 "
+            onClick={() => setOpenPopup(false)}
+          >
+            <FaXmark />
+          </span>
+          <h2 className="font-[600] text-[16px] lg:text-[24px] text-center mb-[30px]">
+            Sevdiklərinizi sevindirin
+          </h2>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className={`${
+              isMobile ? "w-full" : "w-[400px]"
+            }  space-y-[20px] flex flex-col`}
+          >
+            <input
+              {...register("to", { required: true })}
+              placeholder="Kimə"
+              className="rounded-[8px] bg-[#f2f2f2] placeholder:opacity-50 py-[10px] px-[20px] border-none outline-none"
+              onChange={handleInputChange}
+            />
+            <input
+              {...register("from", { required: true })}
+              placeholder="Kimdən"
+              className="rounded-[8px] bg-[#f2f2f2] placeholder:opacity-50 py-[10px] px-[20px] border-none outline-none"
+              onChange={handleInputChange}
+            />
+            <input
+              {...register("message")}
+              placeholder="Şəxsi mesaj"
+              className="rounded-[8px] bg-[#f2f2f2] placeholder:opacity-50 py-[10px] px-[20px] border-none outline-none"
+            />
+            <input
+              {...register("contact", {
+                pattern: /^[0-9]*$/,
+                required: true,
+              })}
+              type="tel"
+              placeholder="Əlaqə nömrəsi"
+              className="rounded-[8px] bg-[#f2f2f2] placeholder:opacity-50 py-[10px] px-[20px] border-none outline-none"
+              onChange={handleInputChange}
+              onPaste={(e) => e.preventDefault()} // Prevent pasting non-numeric values
+            />
+            <button
+              type="submit"
+              className={`mt-[24px] rounded-[8px] h-[48px] ${
+                isFormValid
+                  ? "bg-[#FFCE00] cursor-pointer"
+                  : "bg-[#F1DD8B] cursor-not-allowed"
+              }`}
+              disabled={!isFormValid}
             >
-              <FaXmark />
-            </span>
-            <h2 className="font-[600] text-[24px] text-center mb-[30px]">
-              Sevdiklərinizi sevindirin
-            </h2>
-            <form
-              onSubmit={handleSubmit(onSubmit)}
-              className="w-[400px] space-y-[20px] flex flex-col"
-            >
-              <input
-                {...register("to", { required: true })}
-                placeholder="Kimə"
-                className="rounded-[8px] bg-[#f2f2f2] placeholder:opacity-50 py-[10px] px-[20px] border-none outline-none"
-                onChange={handleInputChange}
-              />
-              <input
-                {...register("from", { required: true })}
-                placeholder="Kimdən"
-                className="rounded-[8px] bg-[#f2f2f2] placeholder:opacity-50 py-[10px] px-[20px] border-none outline-none"
-                onChange={handleInputChange}
-              />
-              <input
-                {...register("message")}
-                placeholder="Şəxsi mesaj"
-                className="rounded-[8px] bg-[#f2f2f2] placeholder:opacity-50 py-[10px] px-[20px] border-none outline-none"
-              />
-              <input
-                {...register("contact", {
-                  pattern: /^[0-9]*$/,
-                  required: true,
-                })}
-                type="tel"
-                placeholder="Əlaqə nömrəsi"
-                className="rounded-[8px] bg-[#f2f2f2] placeholder:opacity-50 py-[10px] px-[20px] border-none outline-none"
-                onChange={handleInputChange}
-                onPaste={(e) => e.preventDefault()} // Prevent pasting non-numeric values
-              />
-              <button
-                type="submit"
-                className={`mt-[24px] rounded-[8px] h-[48px] ${
-                  isFormValid
-                    ? "bg-[#FFCE00] cursor-pointer"
-                    : "bg-[#F1DD8B] cursor-not-allowed"
-                }`}
-                disabled={!isFormValid}
-              >
-                Submit
-              </button>
-            </form>
-          </div>
+              Kartı al
+            </button>
+          </form>
         </div>
-      )}
+      </div>
     </div>
   );
 };
