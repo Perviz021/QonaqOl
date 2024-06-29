57114d8ae14abbc7801de25ddc72b82453911f12
Parviz Namazli <55592138+Perviz021@users.noreply.github.com>
2024-06-29 17:39:06 +0400

    fix: Blog Card component

--- src/pages/home/Blog.jsx
+++ src/pages/home/Blog.jsx
@@ -15,22 +15,22 @@ const Blog = () => {
       </div>
       <div className="w-full grid px-[20px] grid-cols-1 lg:grid-cols-3 gap-[28px]">
         <BlogCard
-          videoId="S8-z26rBPLw?si=R6oRiCNX3Wik6dLj"
+          videoId="K_y6CveztZI?si=T1adcIt0YrZ69T15"
           thumbnailImg={thumbnail1}
-          title="6 Strategies to Find Your Conference Keynote and Other Speakers"
-          description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore laboriosam nesciunt hic ipsam in aspernatur"
+          title="Evini restorana çevirən aşpaz və səyahətçi Yaqub Zeynalzadə"
+          description="Kəndlilərə məhsullarından gəlir əldə etməyi öyrədən, evini restorana çevirərək Bakıda fərqli bir məkan yaradan, İtaliyada aşpazlıq təhsili almış aşpaz və səyahətçi Yaqubun macəraları"
         />
         <BlogCard
-          videoId="OZafwTE1q4Q?si=TGqdisTrUx-lgyFZ"
+          videoId="klSLU23kQeI?si=PVX5TG1cGVXVbrZu"
           thumbnailImg={thumbnail2}
-          title="How Successfully Used Paid Marketing to Drive Incremental Ticket Sales"
-          description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore laboriosam nesciunt hic ipsam in aspernatur"
+          title="Xanbulan gölündə kamp və yeyib içmək"
+          description="Lənkəran rayonu, Hirkan Milli Parkında yerləşən əsrarəngiz mənzərəyə sahib bu göldə kamp etmək üçün sizə nələr lazımdır?"
         />
         <BlogCard
-          videoId="W5b-a8Nzmag?si=0ArwYIiU8qpa3H6v"
+          videoId="K1I7qMa7n9Y?si=khFk5EJQlCV0OKUo"
           thumbnailImg={thumbnail3}
-          title="Introducing Workspaces: Work smarter, not harder with new navigation"
-          description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore laboriosam nesciunt hic ipsam in aspernatur"
+          title="Əsrlərlə yaşı olan ebru sənəti"
+          description="Xüsusi boyalarla hazırlanmış naxışların qatlaşdırılmış su üzərinə köçürülməsi ilə hazırlanan sənət. Azərbaycanda bu sənətlə məşğul olan rəssamları “ebruzən” adlandırırlar"
         />
       </div>
     </section>
